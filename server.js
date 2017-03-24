338c61a9ddb23d90d22c21455a9721440fe2adea
Stanislav Shabalin <Stanislav.Shabalin@exigenservices.com>
2017-03-24 12:38:26 +0200

    Post issue method

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
 const issues = [
     {
         id: 1, status: 'Open', owner: 'Ravan',
-        created: new Date('2016-08-15'), effort: 5, 
+        created: new Date('2016-08-15'), effort: 5,
         completionDate: undefined,
         title: 'Error in console when clicking Add',
     },
@@ -17,6 +19,16 @@ const issues = [
         title: 'Missing bottom border on panel',
     },
 ];
+
+app.post('/api/issues', (req, res) => {
+    const newIssue = req.body;
+    newIssue.id = issues.length + 1;
+    newIssue.created = new Date();
+    if (!newIssue.status)
+        newIssue.status = 'New';
+    issues.push(newIssue);
+    res.json(newIssue);
+});
 
 app.get('/api/issues', (req, res) => {
     const metadata = { total_count: issues.length };
