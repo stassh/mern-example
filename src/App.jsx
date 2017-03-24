f1bde4dbb5b3d5353274e66f77545ac6b76c67e4
Stanislav Shabalin <Stanislav.Shabalin@exigenservices.com>
2017-03-24 12:48:41 +0200

    Use list API

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,6 @@
 
 const contentNode = document.getElementById('contents');
 
-const issues = [
-    {
-        id: 1, status: 'Open', owner: 'Ravan',
-        created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-        title: 'Error in console when clicking Add',
-    },
-    {
-        id: 2, status: 'Assigned', owner: 'Eddie',
-        created: new Date('2016-08-16'), effort: 14,
-        completionDate: new Date('2016-08-30'),
-        title: 'Missing bottom border on panel',
-    },
-];
-
 class IssueFilter extends React.Component {
     render() {
         return (
@@ -58,46 +44,6 @@ function IssueTable(props) {
     );
 }
 
-/*class IssueRow extends React.Component {
-    render() {
-        const issue = this.props.issue;
-        console.log("render() for " + this.className + " has fired");
-        return (
-            <tr>
-                <td>{issue.id}</td>
-                <td>{issue.status}</td>
-                <td>{issue.owner}</td>
-                <td>{issue.created.toDateString()}</td>
-                <td>{issue.effort}</td>
-                <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
-                <td>{issue.title}</td>
-            </tr>
-        )
-    }
-}
-class IssueTable extends React.Component {
-    render() {
-        const borderedStyle = { border: "1px solid silver", padding: 6 };
-        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
-        return (
-            <table className="bordered-table">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Status</th>
-                        <th>Owner</th>
-                        <th>Created</th>
-                        <th>Effort</th>
-                        <th>Completion Date</th>
-                        <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>{issueRows}</tbody>
-            </table>
-        )
-    }
-}*/
-
 class IssueAdd extends React.Component {
 
     constructor() {
@@ -137,7 +83,7 @@ class IssueList extends React.Component {
      * Constructor
      */
     constructor() {
-        super();
+        super(); 
         /** empty state. Will initialize later */
         this.state = { issues: [] };
         //this.createTestIssue = this.createTestIssue.bind(this);
@@ -154,8 +100,21 @@ class IssueList extends React.Component {
     }
 
     loadData() {
-        setTimeout(() => { this.setState({ issues: issues }); }, 500);
+        fetch('/api/issues').then(response =>
+            response.json()
+        ).then(data => {
+            console.log("Total count of records:", data._metadata.total_count);
+            data.records.forEach(issue => {
+                issue.created = new Date(issue.created);
+                if (issue.completionDate)
+                    issue.completionDate = new Date(issue.completionDate);
+            });
+            this.setState({ issues: data.records });
+        }).catch(err => {
+            console.log(err);
+        });
     }
+
 
     createIssue(newIssue) {
         const newIssues = this.state.issues.slice();
